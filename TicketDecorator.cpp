dea320997638b8df9451cbd780517b2686aaa5ff
HP <haythamphillips@gmail.com>
2026-09-07 11:09:41 +0200

    fixed up naming to camel case and alligned with Ori and Haytham Code

---
+++ TicketDecorator.cpp
@@ -0,0 +1,48 @@
+#include "TicketDecorator.h"
+
+TicketDecorator::TicketDecorator(WorkItem* wrapped) : wrapped(wrapped) {}
+
+TicketDecorator::~TicketDecorator() {
+    delete wrapped;
+}
+
+void TicketDecorator::add(WorkItem* child) {
+    if(wrapped!=nullptr){
+        wrapped->add(child);
+    }
+    
+}
+
+
+void TicketDecorator::remove(WorkItem* child) {
+    if(wrapped!=nullptr){
+        wrapped->remove(child);
+    }
+    
+}
+int TicketDecorator::getChildCount() const {
+    return wrapped!=nullptr?wrapped->getChildCount():0;
+}
+
+WorkItem* TicketDecorator::getChildAt(int index) const {
+    return wrapped!= nullptr?wrapped->getChildAt(index):nullptr;
+}
+
+void TicketDecorator::print(int indent) const {
+    if (wrapped!=nullptr) {
+        wrapped->print(indent);
+    }
+}
+
+
+WorkItemIterator* TicketDecorator::createBurndownIterator() const {
+    return wrapped != nullptr ? wrapped->createBurndownIterator():nullptr;
+}
+
+std::string TicketDecorator::getName() const {
+    return wrapped != nullptr ? wrapped->getName() : "";
+}
+
+double TicketDecorator::getCost() const {
+    return wrapped != nullptr ? wrapped->getCost() : 0.0;
+}
---
+++ TicketDecorator.h
@@ -0,0 +1,24 @@
+#ifndef TICKETDECORATOR_H
+#define TICKETDECORATOR_H
+
+#include "WorkItem.h"
+#include <string>
+class TicketDecorator : public WorkItem {
+public:
+    explicit TicketDecorator(WorkItem* wrapped);
+    virtual ~TicketDecorator() override;
+
+    void add(WorkItem* child) override;
+    void remove(WorkItem* child) override;
+    int getChildCount() const override;
+    WorkItem* getChildAt(int index) const override;
+    WorkItemIterator* createBurndownIterator() override;
+    void print(int indent = 0) const override;
+    double getCost() const override;
+    std::string getName() const override;
+
+protected:
+    WorkItem* wrapped;
+};
+
+#endif
